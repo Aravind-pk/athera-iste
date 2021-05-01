8c5c82e39d0f0f1aea98bebbea746156d46a2500
P K Aravindkrishnan <65278075+Aravind-pk@users.noreply.github.com>
2021-05-01 20:25:01 +0530

    final

--- public/script.js
+++ public/script.js
@@ -1,5 +1,5 @@
 
-var eventlist =[
+var allEventlist =[
     "event 1",
     "event 2",
     "event 3",
@@ -8,11 +8,25 @@ var eventlist =[
     "event 6"
 ];
 
+
+var specialEventIndex = 3;
+
+
+
+////////////////////////////////////////////////////////
+
+
 document.getElementById("isteNo").style.display="none";
-var eventOptions = ()=>{
-    evOp =""
-    eventlist.forEach(eventName => {
-        evOp +=  ` <option> ${eventName} </option>  `
+document.getElementById("specialEvent").style.display="none";
+
+var specialEventValue = allEventlist[specialEventIndex]
+var eventsNo=0;
+var dropNo = 0;
+
+var eventOptions = (list)=>{
+    evOp =[];
+    list.forEach(eventName => {
+        evOp.push(` <option> ${eventName} </option>  `);
     });
     return evOp;
 }
@@ -20,15 +34,19 @@ var eventOptions = ()=>{
 
 var isteCheck =(checkbox)=>{
     var isteNo = document.getElementById("isteNo");
+    var coupon = document.getElementById("coupon");
     if(checkbox.checked){
         isteNo.style.display ="block";
+        coupon.style.display ="none";
     }else{
         isteNo.style.display = "none";
+        coupon.style.display = "block";
     }
 }
 
 
 var addEvents = (events) =>{
+    eventsNo = events;
     // console.log(events);
     if( events >3 || events<1 ){
         document.getElementById("eventSelect").value="3";
@@ -40,14 +58,13 @@ var addEvents = (events) =>{
     
         for(i=0; i<events; i++){
             
-    
-    
-            //elements create with ids    event1 ,event2 , event
+       
+            //elements create with ids event1 ,event2 , event
             eventHtml += 
     
-            `     <div class="form-group mb-4" > <select id="event${i + 1}" name=\"account\" class=\"form-control border-0 shadow form-control-lg\">` +
+            `     <div class="form-group mb-4" > <select id="event${i + 1}" name=\"account\" class=\"form-control border-0 shadow form-control-lg\" onChange="validate(event.srcElement.id,value)">` +
             `     <option> Select event ${i+1}</option>  ` +
-                   eventOptions() +
+                   eventOptions(allEventlist).toString() +
             "     </select> </div>  " ;
     
         }
@@ -58,4 +75,69 @@ var addEvents = (events) =>{
     
 };
 
+var selectionList=[];
+var validate=(id, value) =>{
 
+    
+    // var dropIds = []
+    // for(i=0; i<eventsNo; i++){
+    //     dropIds.push(`event${i+1}`)
+    // }
+    // console.log(dropIds)
+
+
+    console.log(id,value)
+
+    if( value == specialEventValue){
+        var kitbox = document.getElementById("specialEvent");
+        kitbox.innerHTML =`Need kit for ${value}` ;
+        kitbox.style.display="block";
+    }
+    console.log( " dddddddddddddddd", eventsNo , "   dd", dropNo)
+    if (dropNo+1 < eventsNo){
+
+            
+     console.log("dorp no " + dropNo);
+            
+                var tempDrop = document.getElementById(`event${dropNo+2}`);
+                console.log (tempDrop , "  ", dropNo+2, "           ////////////")
+                var templist =[];
+                console.log( allEventlist + ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+                templist = templist.concat(allEventlist);
+
+                selectionList.push(value);
+                console.log(templist  +">>>>>>>>>>>>>.")
+
+
+
+                templist = (templist.filter(n => !selectionList.includes(n)))
+
+                // const index = templist.indexOf(value);
+                // if (index > -1) {
+                // templist.splice(index, 1);
+                // }
+
+                // templist = templist.splice(tempDrop.selectedIndex ,1);
+                console.log ("templist"+ templist);
+                tempDrop.innerHTML = 
+               
+                `     <option> Select ${id}</option>  ` +
+                       eventOptions(templist).toString()  ;
+                console.log(eventOptions(templist));
+            
+
+        dropNo++;
+    }
+
+    // var drop = document.getElementById(id);
+    // dropIds.pop(drop);
+
+    // dropIds.forEach(changeDrop);
+
+
+    // console.log(drop)
+    // if(drop.selectedIndex <= 0) {
+    //   alert(`Select an event for ${id}`);
+    // }
+
+}
